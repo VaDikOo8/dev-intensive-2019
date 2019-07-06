7309536933297c3eb40f12e683cb262773f211dc
VaDikOo8 <vadikoo8.patron@gmail.com>
2019-07-06 22:10:09 +0300

    hometask_2 fix stripHtml()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -7,8 +7,7 @@ fun String.truncate(i: Int = 16): String {
 
 fun String.stripHtml(): String {
     return this
-        .replace("</(.+)>".toRegex(), "")
-        .replace("<(.+)>".toRegex(),"")
-        .replace("&(\\S){2,6};".toRegex(),"")
+        .replace("<(.*?)>".toRegex(), "")
+        .replace("&(.*?);".toRegex(),"")
         .replace("[\\s]{2,}".toRegex()," ")
 }
