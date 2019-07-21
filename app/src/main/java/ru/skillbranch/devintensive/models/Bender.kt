d8ec6d37aa54d3f3c340d88410d6f89c17b15264
VaDikOo8 <vadikoo8.patron@gmail.com>
2019-07-21 19:17:58 +0300

    hometask_3 fix after tests

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -12,7 +12,7 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     }
 
     fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
-        val namePattern = "^[А-ЯA-Z][а-яa-z]+(\\s[А-ЯA-Z][а-яa-z]*)?(\\s[А-ЯA-Z][а-яa-z]*)?".toRegex()
+        val namePattern = "^[А-ЯA-Z][а-яa-z]+(\\s[А-ЯA-Zа-яa-z]*)?(\\s[А-ЯA-Zа-яa-z]*)?".toRegex()
         val professionPattern = "^([а-я]+|[a-z]+)\$".toRegex()
         val materialPattern = "^(\\D+)\$".toRegex()
         val birthdayPattern = "^(\\d+)\$".toRegex()
